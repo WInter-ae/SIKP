2e7cc3e0f437504055fa5859e2df2593f674e1ca
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-11-09 17:10:21 +0700

    instal login dan register form dari shadcn

---
+++ app/feature/login/components/login-form.tsx
@@ -0,0 +1,62 @@
+import { cn } from "~/lib/utils"
+import { Button } from "~/components/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "~/components/ui/field"
+import { Input } from "~/components/ui/input"
+import { Github } from "lucide-react"
+import { Link } from "react-router"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to login to your account
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </Field>
+        <Field>
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Link
+              to="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+          <Input id="password" type="password" required />
+        </Field>
+        <Field>
+          <Button type="submit">Login</Button>
+        </Field>
+        <FieldSeparator>Or continue with</FieldSeparator>
+        <Field>
+          <Button variant="outline" type="button">
+            <Github />
+            Login with GitHub
+          </Button>
+          <FieldDescription className="text-center">
+            Belum punya akun?{" "}
+            <Link to="/register" className="underline underline-offset-4">
+              Register
+            </Link>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
+    </form>
+  )
+}
