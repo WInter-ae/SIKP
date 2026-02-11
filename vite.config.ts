82e7a7902b34a64677d2ee08ccfda0f4b56cc362
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2026-02-11 20:55:45 +0700

    ubah port yang digunakan

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    port: 5174,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8787",
